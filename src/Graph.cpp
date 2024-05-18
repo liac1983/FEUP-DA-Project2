67b030d6a60773dcd6e57dfce27ec114f90056a3
Afonso Neves <up202108884@up.pt>
2024-05-18 16:27:09 +0100

    updated Graph data structure

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -1,17 +1,14 @@
 #include "Graph.h"
 
-#include <limits>
-#include <fstream>
-#include <sstream>
-
 using namespace std;
 
 /* Constructor */
 
 Graph::Graph() {
     numVertices = 0;
-    visited = vector<bool>(numVertices, false);
-    graphMatrix = vector<vector<double>>(numVertices, vector<double>(numVertices, 0));
+    visited = vector<bool>();
+    graphMatrix = vector<vector<double>>();
+    nodeCoordinates = unordered_map<int, pair<double, double>>();
 }
 
 /* Getters */
@@ -28,6 +25,10 @@ const vector<vector<double>> &Graph::getGraphMatrix() const {
     return graphMatrix;
 }
 
+const unordered_map<int, pair<double, double>> &Graph::getNodeCoordinates() const {
+    return nodeCoordinates;
+}
+
 /* Setters */
 
 void Graph::setNumVertices(int newNumVertices) {
@@ -42,118 +43,6 @@ void Graph::setGraphMatrix(const vector<vector<double>> &newGraphMatrix) {
     Graph::graphMatrix = newGraphMatrix;
 }
 
-/* Functions */
-
-void Graph::addEdge(int src, int dest, double distance)
-{
-    graphMatrix[src][dest] = distance;
+void Graph::setNodeCoordinates(const unordered_map<int, pair<double, double>> &newNodeCoordinates) {
+    Graph::nodeCoordinates = newNodeCoordinates;
 }
-
-void Graph::removePipe(int src, int dest)
-{
-    graphMatrix[src][dest] = 0;
-}
-
-double Graph::getEdgeValue(int src, int dest)
-{
-    return graphMatrix[src][dest];
-}
-
-void filterString(string& input) {
-    string temp;
-    for (char c : input)
-        if (isdigit(c) || c == '.')
-            temp += c;
-    input = temp;
-}
-
-void Graph::loadGraph(const string &fileLocation) {
-    fstream file;
-    file.open(fileLocation, ios::in);
-    if (!file.is_open())
-        throw runtime_error("ERROR - The function \"loadGraph\" could not read the graph data");
-
-    string line;
-    getline(file, line); // Skip first line
-
-    int maxVertex = -1;
-    string src, dest, dist;
-
-    // First pass to find the maximum vertex number
-    while (getline(file, line)) {
-        if (line.empty()) continue;
-
-        istringstream ss(line);
-
-        getline(ss, src, ',');
-        getline(ss, dest, ',');
-        getline(ss, dist);
-
-        if (src.empty() || dest.empty()) continue;
-
-        maxVertex = max(maxVertex, max(stoi(src), stoi(dest)));
-    }
-
-    numVertices = maxVertex + 1;
-    visited = vector<bool>(numVertices, false);
-    graphMatrix = vector<vector<double>>(numVertices, vector<double>(numVertices, 0));
-    file.close();
-
-    file.open(fileLocation, ios::in);
-    getline(file, line); // Skip first line
-
-    // Second pass to fill the graphMatrix
-    while (getline(file, line)) {
-        if (line.empty()) continue;
-
-        istringstream ss(line);
-
-        getline(ss, src, ',');
-        getline(ss, dest, ',');
-        getline(ss, dist);
-
-        filterString(dist);
-
-        if (src.empty() || dest.empty()) continue;
-
-        graphMatrix[stoi(src)][stoi(dest)] = stof(dist);
-        graphMatrix[stoi(dest)][stoi(src)] = stof(dist);
-    }
-
-    file.close();
-}
-
-void Graph::TSPUtil(int node, int count, double cost, double &minCost, vector<int> &path, vector<int> &optimalPath) {
-    visited[node] = true;
-    path.push_back(node);
-
-    if (count == numVertices) {
-        // If all nodes are visited, add the cost of the edge from the last node to the first node
-        cost += getEdgeValue(path.back(), path.front());
-        if (cost < minCost) {
-            minCost = cost;
-            optimalPath = path;
-        }
-    } else {
-        for (int i = 0; i < numVertices; i++) {
-            if (!visited[i] && cost + getEdgeValue(node, i) < minCost) {
-                TSPUtil(i, count + 1, cost + getEdgeValue(node, i), minCost, path, optimalPath);
-            }
-        }
-    }
-
-    visited[node] = false;
-    path.pop_back();
-}
-
-pair<double, vector<int>> Graph::TSP() {
-    vector<int> path;
-    vector<int> optimalPath;
-    double minCost = numeric_limits<double>::infinity();
-
-    TSPUtil(0, 1, 0, minCost, path, optimalPath);
-    optimalPath.push_back(0);
-
-    return {minCost, optimalPath};
-}
-
--- src/Graph.h
+++ src/Graph.h
@@ -3,9 +3,10 @@
 
 #include <string>
 #include <vector>
+#include <unordered_map>
 
 /**
- * @brief Class representing a graph data structure.
+ * @brief Class representing the graph's data structure.
  */
 class Graph {
 public:
@@ -18,39 +19,27 @@ public:
     int getNumVertices() const;
     const std::vector<bool> &getVisited() const;
     const std::vector<std::vector<double>> &getGraphMatrix() const;
+    const std::unordered_map<int, std::pair<double, double>> &getNodeCoordinates() const;
 
     /* Setters */
     void setNumVertices(int numVertices);
     void setVisited(const std::vector<bool> &visited);
     void setGraphMatrix(const std::vector<std::vector<double>> &graphMatrix);
+    void setNodeCoordinates(const std::unordered_map<int, std::pair<double, double>> &nodeCoordinates);
 
-    /* Functions */
-    /**
-     * @brief Add an edge to the graph
-     * @param src The source node of the edge.
-     * @param dest The destination node of the edge.
-     * @param cap The distance of the edge.
-     */
-    void addEdge(int src, int dest, double distance);
-    /**
-     * @brief Remove an edge from the graph.
-     * @param src The source node of the edge.
-     * @param dest The destination node of the edge.
-     */
-    void removePipe(int src, int dest);
-    /**
-     * @brief Get the distance value of an edge.
-     * @param src The source node of the edge.
-     * @param dest The destination node of the edge.
-     * @return The distance value of the edge.
-     */
-    double getEdgeValue(int src, int dest);
+    /* Parsing */
     /**
      * @brief Load the graph with the contents from a file.
      * @param fileLocation The location of the file containing the graph data.
      */
-    static void loadGraph(const std::string &fileLocation);
+    void loadGraph(const std::string &fileLocation);
+    /**
+     * @brief Load the extra large graphs with the contents from a file.
+     * @param fileLocation The location of the file containing the graph data.
+     */
+    void loadExtraLargeGraph(const std::string &fileLocation);
 
+    /* Graph Operations */
     void TSPUtil(int node, int count, double cost, double &minCost, std::vector<int> &path, std::vector<int> &optimalPath);
     std::pair<double, std::vector<int>> TSP();
 
@@ -59,6 +48,7 @@ private:
     int numVertices;
     std::vector<bool> visited;
     std::vector<std::vector<double>> graphMatrix;
+    std::unordered_map<int,std::pair<double,double>> nodeCoordinates;
 
 };
 
